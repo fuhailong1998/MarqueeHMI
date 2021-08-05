2d52acd95ed58c082f26371d2fae3d04e30033e5
hailong.fu <hailong.fu@thundersoft.com>
2021-08-05 18:24:51 +0800

    light is ok,muliprocess is not ok

--- app/src/main/java/com/example/marquee/FirstActivity.java
+++ app/src/main/java/com/example/marquee/FirstActivity.java
@@ -1,41 +1,32 @@
 package com.example.marquee;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author leon
+ */
 public class FirstActivity extends AppCompatActivity {
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.add_item:
-                Toast.makeText(this, "ADD", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.remove_item:
-                Toast.makeText(this, "REMOVE", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-        }
-        return true;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,18 +34,14 @@ public class FirstActivity extends AppCompatActivity {
         return true;
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
-
-
         ActionBar actionbar = getSupportActionBar();
-        if (actionbar != null){
+        if (actionbar != null) {
             actionbar.hide();
         }
-
         View decorView = getWindow().getDecorView();
         // Hide both the navigation bar and the status bar.
         // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
@@ -64,10 +51,81 @@ public class FirstActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+    }
+
+    boolean power = false;
+    boolean marqueeFlag = false;
+
+    public void light(Boolean[] b, TextView[] t) {
+
+            for (int i = 0; i < 4; i++) {
+                switch (i) {
+                    case 0:
+                        if (b[i]) {
+                            if (power){
+                                t[i].setBackgroundResource(R.drawable.lightred);
+                            }
+                        } else {
+                            t[i].setBackgroundResource(R.drawable.light);
+                        }
+                        ;
+                        break;
+                    case 1:
+                        if (b[i]) {
+                            if (power){
+                                t[i].setBackgroundResource(R.drawable.lightgreen);
+                            }
+                        } else {
+                            t[i].setBackgroundResource(R.drawable.light);
+                        }
+                        ;
+                        break;
+                    case 2:
+                        if (b[i]) {
+                            if (power){
+                                t[i].setBackgroundResource(R.drawable.lightblue);
+                            }
+                        } else {
+                            t[i].setBackgroundResource(R.drawable.light);
+                        }
+                        ;
+                        break;
+                    case 3:
+                        if (b[i]) {
+                            if (power){
+                                t[i].setBackgroundResource(R.drawable.lightup);
+                            }
+                        } else {
+                            t[i].setBackgroundResource(R.drawable.light);
+                        }
+                        ;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         TextView t1 = findViewById(R.id.textView4);
         TextView t2 = findViewById(R.id.textView6);
         TextView t3 = findViewById(R.id.textView5);
         TextView t4 = findViewById(R.id.textView7);
+        TextView[] leds = {t1, t2, t3, t4};
+        SeekBar s1 = findViewById(R.id.seekBar3);
+        SeekBar s2 = findViewById(R.id.seekBar4);
+        SeekBar s3 = findViewById(R.id.seekBar5);
+        SeekBar s4 = findViewById(R.id.seekBar6);
+        SeekBar[] freqs = {s1, s2, s3, s4};
+        TextView ft1 = findViewById(R.id.textView);
+        TextView ft2 = findViewById(R.id.textView14);
+        TextView ft3 = findViewById(R.id.textView13);
+        TextView ft4 = findViewById(R.id.textView16);
+        TextView[] fts = {ft1, ft2, ft3, ft4};
         Button button1 = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch powerBtn = findViewById(R.id.switch6);
@@ -76,6 +134,19 @@ public class FirstActivity extends AppCompatActivity {
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led2 = findViewById(R.id.switch3);
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led3 = findViewById(R.id.switch4);
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led4 = findViewById(R.id.switch5);
+        Switch[] ss = {led1, led2, led3, led4};
+        Boolean[] ledStatus = {false, false, false, false};
+
+
+
+        Boolean[][] ledMarquee = {
+                {true, false, false, false},
+                {false, true, false, false},
+                {false, false, true, false},
+                {false, false, false, true},
+        };
+
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,7 +158,7 @@ public class FirstActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(FirstActivity.this,"Good Bye!",Toast.LENGTH_SHORT).show();
+                Toast.makeText(FirstActivity.this, "Good Bye!", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
@@ -96,78 +167,80 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    Toast.makeText(FirstActivity.this,"Power On",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FirstActivity.this, "Power On", Toast.LENGTH_SHORT).show();
+                    power = true;
+                    light(ledStatus, leds);
                 } else {
-                    Toast.makeText(FirstActivity.this,"Power Off",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FirstActivity.this, "Power Off", Toast.LENGTH_SHORT).show();
+                    power = false;
+                    for (int i = 0; i < 4; i++) {
+                        leds[i].setBackgroundResource(R.drawable.light);
+                    }
                 }
             }
         });
+
+
 
         marqueeBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    Toast.makeText(FirstActivity.this,"Marquee On",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FirstActivity.this, "Marquee On", Toast.LENGTH_SHORT).show();
+                    marqueeFlag = true;
+
                 } else {
-                    Toast.makeText(FirstActivity.this,"Marquee Off",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(FirstActivity.this, "Marquee Off", Toast.LENGTH_SHORT).show();
+                    marqueeFlag = false;
+
                 }
             }
         });
 
 
+        for (int i = 0; i < 4; i++) {
 
-        led1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(FirstActivity.this,"LED1 On",Toast.LENGTH_SHORT).show();
-                    t1.setBackgroundResource(R.drawable.lightup);
-                } else {
-                    Toast.makeText(FirstActivity.this,"LED1 Off",Toast.LENGTH_SHORT).show();
-                    t1.setBackgroundResource(R.drawable.light);
+            int finalI = i;
+            ss[i].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        Toast.makeText(FirstActivity.this, "LED"+ finalI +1+" On", Toast.LENGTH_SHORT).show();
+                        ledStatus[finalI] = true;
+                        if (power){
+                            light(ledStatus, leds);
+                        }
+                    } else {
+                        Toast.makeText(FirstActivity.this, "LED1"+ finalI +1+" Off", Toast.LENGTH_SHORT).show();
+                        ledStatus[finalI] = false;
+                        light(ledStatus, leds);
+                    }
+                }
+            });
+        }
+
+
+        for (int i = 0; i < 4; i++) {
+            int ii = i;
+            freqs[i].setOnSeekBarChangeListener((new SeekBar.OnSeekBarChangeListener() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    fts[ii].setText(""+i);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
 
                 }
-            }
-        });
 
-        led2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(FirstActivity.this,"LED2 On",Toast.LENGTH_SHORT).show();
-                    t2.setBackgroundResource(R.drawable.lightup);
-                } else {
-                    Toast.makeText(FirstActivity.this,"LED2 Off",Toast.LENGTH_SHORT).show();
-                    t2.setBackgroundResource(R.drawable.light);
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
                 }
-            }
-        });
-
-        led3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(FirstActivity.this,"LED3 On",Toast.LENGTH_SHORT).show();
-                    t3.setBackgroundResource(R.drawable.lightup);
-                } else {
-                    Toast.makeText(FirstActivity.this,"LED3 Off",Toast.LENGTH_SHORT).show();
-                    t3.setBackgroundResource(R.drawable.light);
-                }
-            }
-        });
-
-        led4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(FirstActivity.this,"LED4 On",Toast.LENGTH_SHORT).show();
-                    t4.setBackgroundResource(R.drawable.lightup);
-                } else {
-                    Toast.makeText(FirstActivity.this,"LED4 Off",Toast.LENGTH_SHORT).show();
-                    t4.setBackgroundResource(R.drawable.light);
-                }
-            }
-        });
-
+            }));
+        }
     }
+
+
 }
