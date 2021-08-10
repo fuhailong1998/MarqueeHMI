b30303dea25021ba6e92c3960847362399e3620a
hailong.fu <hailong.fu@thundersoft.com>
2021-08-10 18:20:34 +0800

    6.611

--- app/src/main/java/com/example/marquee/FirstActivity.java
+++ app/src/main/java/com/example/marquee/FirstActivity.java
@@ -15,14 +15,26 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author leon
  */
 public class FirstActivity extends AppCompatActivity {
 
+    private ScheduledExecutorService scheduleTaskExecutor;
+    //多线程并行处理定时任务时，Timer运行多个TimeTask时，只要其中之一没有捕获抛出的异常，其它任务便会自动终止运行，使用ScheduledExecutorService则没有这个问题。
+    //ScheduledExecutorService,是基于线程池设计的定时任务类,每个调度任务都会分配到线程池中的一个线程去执行,也就是说,任务是并发执行,互不影响。
+    //只有当调度任务来的时候,ScheduledExecutorService才会真正启动一个线程,其余时间ScheduledExecutorService都是出于轮询任务的状态。
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,10 +52,6 @@ public class FirstActivity extends AppCompatActivity {
             actionbar.hide();
         }
         View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
@@ -111,8 +119,8 @@ public class FirstActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         TextView[] leds = {findViewById(R.id.textView4), findViewById(R.id.textView6), findViewById(R.id.textView5), findViewById(R.id.textView7)};
-        SeekBar[] freqs = {findViewById(R.id.seekBar3), findViewById(R.id.seekBar5), findViewById(R.id.seekBar4), findViewById(R.id.seekBar6)};
-        TextView[] fts = {findViewById(R.id.textView), findViewById(R.id.textView13), findViewById(R.id.textView14), findViewById(R.id.textView16)};
+        SeekBar[] freqs = {findViewById(R.id.seekBar3), findViewById(R.id.seekBar5), findViewById(R.id.seekBar4), findViewById(R.id.seekBar6), findViewById(R.id.seekBar2)};
+        TextView[] fts = {findViewById(R.id.textView), findViewById(R.id.textView13), findViewById(R.id.textView14), findViewById(R.id.textView16), findViewById(R.id.textView9)};
         Button button1 = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch powerBtn = findViewById(R.id.switch6);
@@ -123,38 +131,11 @@ public class FirstActivity extends AppCompatActivity {
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led4 = findViewById(R.id.switch5);
         Switch[] ss = {led1, led2, led3, led4};
         Boolean[] ledStatus = {false, false, false, false};
-
-        class Mhandler extends Handler {
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case 1:
-                        leds[0].setBackgroundResource(R.drawable.lightred);
-                        break;
-                    case 2:
-                        leds[0].setBackgroundResource(R.drawable.light);
-                        break;
-                }
-            }
-        }
         Boolean[][] ledMarquee = {
                 {true, false, false, false},
                 {false, true, false, false},
                 {false, false, true, false},
                 {false, false, false, true},
-        };
-
-        Handler timerHandler = new Handler();
-        Runnable timerRunnable = new Runnable() {
-
-            Message msg = new Message();
-
-            @Override
-            public void run() {
-                Log.d("TAG", "run: ");
-
-                timerHandler.postDelayed(this, 500);
-            }
         };
 
         button1.setOnClickListener(new View.OnClickListener() {
@@ -188,28 +169,58 @@ public class FirstActivity extends AppCompatActivity {
                 }
             }
         });
+
+        @SuppressLint("HandlerLeak") Handler mHandler = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case 0:
+                        Log.d("TAG", "handleMessage: "+msg.arg1);
+                        leds[msg.what].setBackgroundResource(R.drawable.lightred);
+                        break;
+                    case 1:
+                        Log.d("TAG", "handleMessage: "+msg.what);
+                        leds[msg.what].setBackgroundResource(R.drawable.lightgreen);
+                        break;
+                    case 2:
+                        Log.d("TAG", "handleMessage: "+msg.what);
+                        leds[msg.what].setBackgroundResource(R.drawable.lightblue);
+                        break;
+                    case 3:
+                        Log.d("TAG", "handleMessage: "+msg.what);
+                        leds[msg.what].setBackgroundResource(R.drawable.lightup);
+                        break;
+                    default:
+                        Log.d("TAG", "handleMessage: "+msg.what);
+
+                        break;
+                }
+
+
+            }
+        };
+        scheduleTaskExecutor = new ScheduledThreadPoolExecutor(5, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r);
+            }
+        });
+
         marqueeBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     Toast.makeText(FirstActivity.this, "Marquee On", Toast.LENGTH_SHORT).show();
                     marqueeFlag = true;
-                    new Thread() {
-                        @Override
-                        public void run() {
-                            while (power) {
-                                for (Boolean[] status : ledMarquee
-                                ) {
-                                    try {
-                                        Thread.sleep(500);
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                    light(status, leds);
-                                }
+                        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+                            @Override
+                            public void run() {
+                                Message message = new Message();
+                                message.what = 1;
+                                mHandler.sendMessage(message);
                             }
-                        }
-                    }.start();
+                        }, 0, 500, TimeUnit.MICROSECONDS);
 
                 } else {
                     Toast.makeText(FirstActivity.this, "Marquee Off", Toast.LENGTH_SHORT).show();
@@ -218,6 +229,8 @@ public class FirstActivity extends AppCompatActivity {
                 }
             }
         });
+
+//        LED开关
         for (int i = 0; i < 4; i++) {
 
             int finalI = i;
@@ -238,7 +251,8 @@ public class FirstActivity extends AppCompatActivity {
                 }
             });
         }
-        for (int i = 0; i < 4; i++) {
+//        检测Switch滑动
+        for (int i = 0; i < 5; i++) {
             int ii = i;
             freqs[i].setOnSeekBarChangeListener((new SeekBar.OnSeekBarChangeListener() {
                 @SuppressLint("SetTextI18n")
@@ -256,101 +270,26 @@ public class FirstActivity extends AppCompatActivity {
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
                     if (power) {
-//                        switch (finalI1) {
-//                            case 0:
-//                                new Thread() {
-//                                    @Override
-//                                    public void run() {
-//                                        while (power) {
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.lightred);
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.light);
-//                                        }
-//                                    }
-//                                }.start();
-//                                break;
-//                            case 1:
-//                                new Thread() {
-//                                    @Override
-//                                    public void run() {
-//                                        while (power) {
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.lightgreen);
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.light);
-//                                        }
-//                                    }
-//                                }.start();
-//                                break;
-//                            case 2:
-//                                new Thread() {
-//                                    @Override
-//                                    public void run() {
-//                                        while (power) {
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.lightblue);
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.light);
-//                                        }
-//                                    }
-//                                }.start();
-//                                break;
-//                            case 3:
-//                                new Thread() {
-//                                    @Override
-//                                    public void run() {
-//                                        while (power) {
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.lightup);
-//                                            try {
-//                                                Thread.sleep(seekBar.getProgress());
-//                                            } catch (InterruptedException e) {
-//                                                e.printStackTrace();
-//                                            }
-//                                            leds[finalI].setBackgroundResource(R.drawable.light);
-//                                        }
-//                                    }
-//                                }.start();
-//                                break;
-//                            default:
-//                                break;
-//                        }
-
-
-                        timerHandler.removeCallbacks(timerRunnable);
-                        timerHandler.postDelayed(timerRunnable, 500);
+                        scheduleTaskExecutor.schedule(new Runnable() {
+                            @Override
+                            public void run() {
+                                Message message = new Message();
+                                message.what = ii;
+                                message.arg1 = seekBar.getProgress();
+                                mHandler.sendMessage(message);
+                            }
+                        },0L,TimeUnit.SECONDS);
                     }
                 }
             }));
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (scheduleTaskExecutor != null && !scheduleTaskExecutor.isShutdown()) {
+            scheduleTaskExecutor.shutdown();
         }
     }
 }
